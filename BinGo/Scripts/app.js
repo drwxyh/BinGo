64f653ba2d733a727be43a5167dc800652fa30f6
xuyuhang <drwxyh@outlook.com>
2018-03-15 12:48:58 +0800

    User界面修改

--- BinGo/Scripts/app.js
+++ BinGo/Scripts/app.js
@@ -88,7 +88,19 @@ app.controller('UserInfoCtrl', function ($scope, $http) {
         // 获取用户浏览历史
 
         // 获取用户推荐列表
-
+        //$http({
+        //    method: 'POST',
+        //    url: 'http://127.0.0.1:8088/user/get_recommend.do',
+        //    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        //    data: $.param({})
+        //}).then(function successCallback(response) {
+        //        if (response.data.status === 0) {
+        //            $scope.recommendMovies = response.data.data;
+        //        }
+        //    },
+        //    function errorCallback(errorresponse) {
+        //        alert(errorresponse.data.msg);
+        //    });
     };
     // 修改用户信息相关函数
     $scope.Modify = function () {
@@ -119,7 +131,102 @@ app.controller('UserInfoCtrl', function ($scope, $http) {
 });
 
 app.controller('UserManageCtrl', function ($scope, $http) {
-    // 用户修改自己的信息
+    // 存储用户信息
+    $scope.movies = [];
+    $scope.selPage = 1;
+    // 获取用户列表
+    $http({
+        method: 'POST',
+        url: 'http://127.0.0.1:8088/user/list.do',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        data: $.param({
+            pageSize: 10,
+            pageNum:1
+})
+    }).then(function successCallback(response) {
+        alert(response.data.msg);
+        if (response.data.status === 0) {
+            $scope.movies = response.data.list;
+            $scope.pages = response.data.pages;
+            $scope.newPages = $scope.pages > 5 ? 5 : $scope.pages;
+            $scope.pageList = []; 
+        }
+    }, function errorCallback(errorresponse) {
+        alert(errorresponse.data.msg);
+        });
+
+    //分页要repeat的数组
+    for (var i = 0; i < $scope.NewPages; i++) {
+        $scope.pageList.push(i + 1);
+    }
+    //打印当前选中页索引
+    $scope.selectPage = function (page) {
+        //不能小于1大于最大
+        if (page < 1 || page > $scope.pages) return;
+        //最多显示分页数5
+        if (page > 2) {
+            //因为只显示5个页数，大于2页开始分页转换
+            var newpageList = [];
+            for (var i = (page - 3); i < ((page + 2) > $scope.pages ? $scope.pages : (page + 2)); i++) {
+                newpageList.push(i + 1);
+            }
+            $scope.pageList = newpageList;
+        }
+        $scope.selPage = page;
+        $scope.setData();
+        $scope.isActivePage(page);
+        console.log("选择的页：" + page);
+    };
+    //设置当前选中页样式
+    $scope.isActivePage = function (page) {
+        return $scope.selPage == page;
+    };
+    //上一页
+    $scope.Previous = function () {
+        $http({
+            method: 'POST',
+            url: 'http://127.0.0.1:8088/user/list.do',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            data: $.param({
+                pageSize: 10,
+                pageNum: $scope.selPage - 1
+            })
+        }).then(function successCallback(response) {
+            alert(response.data.msg);
+            if (response.data.status === 0) {
+                $scope.movies = response.data.list;
+                $scope.pages = response.data.pages;
+                $scope.newPages = $scope.pages > 5 ? 5 : $scope.pages;
+                $scope.pageList = [];
+                $scope.selPage = 1;
+            }
+        }, function errorCallback(errorresponse) {
+            alert(errorresponse.data.msg);
+        });
+    }
+    //下一页
+    $scope.Next = function () {
+        $http({
+            method: 'POST',
+            url: 'http://127.0.0.1:8088/user/list.do',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            data: $.param({
+                pageSize: 10,
+                pageNum: $scope.selPage + 1
+            })
+        }).then(function successCallback(response) {
+            alert(response.data.msg);
+            if (response.data.status === 0) {
+                $scope.movies = response.data.list;
+                $scope.pages = response.data.pages;
+                $scope.newPages = $scope.pages > 5 ? 5 : $scope.pages;
+                $scope.pageList = [];
+                $scope.selPage = 1;
+            }
+        }, function errorCallback(errorresponse) {
+            alert(errorresponse.data.msg);
+        });
+    };
 });
 
 app.controller('QuestionniareCtrl', function($scope, $http) {
